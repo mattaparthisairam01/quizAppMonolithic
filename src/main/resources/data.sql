3414ab5dbd27ced06cd800b4ff9ef47e0c5b8735
mattaparthisairam01 <mattaparthisairam01@gmail.com>
2026-05-07 23:51:13 +0530

    getallquestions, getquestionbycategory and post question done

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO question (category, difficulty_level,option1,option2,option3,option4,question_title,right_answer)
+VALUES('Java','Easy','1 byte','2 bytes','3 bytes','4 bytes','What is the size of integer?','4 bytes');
